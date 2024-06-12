4b3f65ec411b35bd395744848f1270e2a1a1ffe5
José Carlos <josecarlosperezgonzalez200@gmail.com>
2024-06-12 11:08:12 +0200

    prueba 50003

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ sudo docker pull josecarlospg2907/backend_pgp:12.06.1
 sudo docker network create --driver bridge --subnet 172.20.0.0/16 --gateway 172.20.0.1 red_pgp
 
 # Creas el contenedor en base a la imagen que descargamos anteriormente
-sudo docker run -d --name backend -p 8081:80 -p 8082:3306 -p 8083:8000 --ip 172.20.0.2 --net red_pgp josecarlos2907/backend_pgp:12.06.1
+sudo docker run -d --name backend -p 8081:80 -p 8082:3306 -p 8083:8000 --ip 172.20.0.2 --net red_pgp josecarlospg2907/backend_pgp:12.06.1
 
 sleep 5
 
