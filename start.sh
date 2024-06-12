d8e1dc36c0221860d4b09c68d6ad391e8d012146
José Carlos <josecarlosperezgonzalez200@gmail.com>
2024-06-12 11:06:42 +0200

    prueba 50002

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 sudo docker pull josecarlospg2907/backend_pgp:12.06.1
 
 # Creas la red para el contenedor
-sudo docker network create -–driver bridge -–subnet 172.20.0.0/16 -–gateway 172.20.0.1 red_pgp
+sudo docker network create --driver bridge --subnet 172.20.0.0/16 --gateway 172.20.0.1 red_pgp
 
 # Creas el contenedor en base a la imagen que descargamos anteriormente
-sudo docker run -d –-name backend -p 8081:80 -p 8082:3306 -p 8083:8000 -–ip 172.20.0.2 -–net red_pgp josecarlos2907/backend_pgp:12.06.1
+sudo docker run -d --name backend -p 8081:80 -p 8082:3306 -p 8083:8000 --ip 172.20.0.2 --net red_pgp josecarlos2907/backend_pgp:12.06.1
 
 sleep 5
 
@@ -13,7 +13,7 @@ sleep 5
 
 COMANDO1 = "/opt/lampp/lampp start"
 COMANDO2 = "cd /home/usuario/symfony/API_PortalGP"
-COMANDO3 = "symfony server:start –-port=8000"
+COMANDO3 = "symfony server:start --port=8000"
 
 docker exec backend /bin/sh -c "$COMANDO1 && $COMANDO2 && $COMANDO3"
 
