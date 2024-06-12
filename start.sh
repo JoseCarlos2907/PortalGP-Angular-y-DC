86f4793f6daa7b6fef1a4055d1462488c583c1cd
José Carlos <josecarlosperezgonzalez200@gmail.com>
2024-06-12 10:49:36 +0200

    un poco modificado

--- start.sh
+++ start.sh
@@ -5,14 +5,21 @@ sudo docker pull josecarlos2907/backend_pgp:12.06.1
 sudo docker network create -–driver bridge -–subnet 172.20.0.0/16 -–gateway 172.20.0.1 red_pgp
 
 # Creas el contenedor en base a la imagen que descargamos anteriormente
-sudo docker run -it –-name backend -p 8081:80 -p 8082:3306 -p 8083:8000 -–ip 172.20.0.2 -–net red_pgp josecarlos2907/backend_pgp:12.06.1
+sudo docker run -d –-name backend -p 8081:80 -p 8082:3306 -p 8083:8000 -–ip 172.20.0.2 -–net red_pgp josecarlos2907/backend_pgp:12.06.1
+
+sleep 5
 
 # Se ejecutan estos comandos para abrir el servicio y poner en funcionamiento la API
-/opt/lampp/lampp start
-symfony server:start –-port=8000
+
+COMANDO1 = "/opt/lampp/lampp start"
+COMANDO2 = "symfony server:start –-port=8000"
+
+docker exec backend/bin/sh -c "$COMANDO1 && $COMANDO2"
 
 # Se ejecutan estos comandos para arrancar la aplicación
-cd PortalGP
+git pull https://github.com/JoseCarlos2907/Angular_PortalGP.git
+
+cd Angular_PortalGP
 npm i
 ng s
 
